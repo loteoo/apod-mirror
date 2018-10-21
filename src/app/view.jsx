d7a7928a62e76662f6805cffee44bb297abb2095
Alexandre Lotte <alexlotte16@gmail.com>
2018-10-21 01:17:46 -0400

    css

--- src/app/view.jsx
+++ src/app/view.jsx
@@ -5,13 +5,13 @@ import {h} from 'hyperapp'
 
 // Import actions
 import {Navigate, ToggleSidebar} from './actions'
-import {LeftArrow, RightArrow, Info, Close, Github} from './icons'
+import {LeftArrow, RightArrow, Info, Close, Github, External} from './icons'
 import {formatDateString, relativeDateString} from './utils'
 
 // Root view
 export const view = (
   state,
-  picture = state.pictures[state.path] || {},
+  picture = state.pictures[state.path],
   endReached = state.path >= state.today
 ) => (
   <div className={'app' + (state.sidebarOpened ? ' sidebar-opened' : '')}>
@@ -21,6 +21,7 @@ export const view = (
     </header>
     <aside>
       <a onclick={ToggleSidebar}><Close /></a>
+      {picture ?
       <div className="content">
         <h2>{picture.title}</h2>
         <h4>
@@ -28,15 +29,20 @@ export const view = (
           {formatDateString(picture.date)} <br />
         </h4>
         <p>{picture.explanation}</p>
+        <br />
+        <a href={'https://apod.nasa.gov/apod/ap' + picture.date.substring(2).replace(/-/g, '') + '.html'} target="_blank"><External /></a>
       </div>
+      : null}
     </aside>
     <main>
       {
-        picture.media_type === 'image'
-          ? <a key={picture.date} href={picture.hdurl || picture.url} target="_blank"><img src={picture.url} alt={picture.title} /></a>
-          : picture.media_type === 'video'
-            ? <div className="youtube"><iframe width="560" height="315" src={picture.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
-            : null
+        !picture 
+          ? <p>Loading...</p>
+          : picture.media_type === 'image'
+            ? <a key={picture.date} href={picture.hdurl || picture.url} target="_blank"><img src={picture.url} alt={picture.title} /></a>
+            : picture.media_type === 'video'
+              ? <div className="youtube"><iframe width="560" height="315" src={picture.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
+              : <p>Media not found</p>
       }
       <a className="prev" href={window.location.pathname + '#/' + relativeDateString(state.path, -1)}><LeftArrow /></a>
       {!endReached ? <a className="next" href={window.location.pathname + '#/' + relativeDateString(state.path, 1)}><RightArrow /></a> : null}
