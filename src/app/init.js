599207d950c7d374b4a4fcc335f6a70b75c7e489
Alexandre Lotte <alexlotte16@gmail.com>
2018-10-21 00:39:00 -0400

    cleaner init

--- src/app/init.js
+++ src/app/init.js
@@ -1,19 +1,14 @@
 
-import {BatchEffects, FetchPicture, relativeDateString} from './utils'
+import {Navigate} from './actions'
 
 const today = new Date().toISOString().split('T')[0]
 
 const path = window.location.hash.substring(2) || today
+
 // Initial state of the app
-export const init = [{
+export const init = Navigate({
   today,
   path,
   sidebarOpened: window.innerWidth > 640,
   pictures: {}
-},
-  BatchEffects([
-    FetchPicture({date: path}),
-    FetchPicture({date: relativeDateString(path, -1)}),
-    today !== path ? FetchPicture({date: relativeDateString(path, 1)}) : null
-  ])
-]
+}, path)
