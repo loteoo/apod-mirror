bc3fac4118ca459c5165ed3298c263ffcbb7fec1
Alexandre Lotte <alexlotte16@gmail.com>
2018-10-30 00:05:39 -0400

    cleanup utils

--- src/app/utils.js
+++ src/app/utils.js
@@ -2,57 +2,43 @@
 import {SetPicture} from './actions'
 
 // Batch effects utility
-const batchEffect = (props, dispatch) => 
-  props.effects.forEach(effect => effect && effect.effect(effect, dispatch))
-
 export const BatchEffects = (effects) => ({
   effects,
-  effect: batchEffect
+  effect: (props, dispatch) => 
+    props.effects.forEach(effect => effect && effect.effect(effect, dispatch))
 })
 
-
-
-
 // Fetch picture effect
-const fetchEffect = (props, dispatch) =>
-  fetch(`https://api.nasa.gov/planetary/apod?date=${props.date}&api_key=8dUEsh65unCXLDx00RqiRtURx5DNLPSRCtbsJ8v2`)
-    .then(response => response.json())
-    .then(picture => {
-
-      // Dispatch action
-      dispatch(SetPicture, {date: props.date, picture})
-
-      // Pre-load image in the browser
-      new Image().src = picture.url
-    })
-    .catch(err => console.log('Fetch error: ', err))
-
 export const FetchPicture =  (props) => ({
-  effect: fetchEffect,
+  effect: (props, dispatch) =>
+    fetch(`https://api.nasa.gov/planetary/apod?date=${props.date}&api_key=8dUEsh65unCXLDx00RqiRtURx5DNLPSRCtbsJ8v2`)
+      .then(response => response.json())
+      .then(picture => {
+
+        // Dispatch action
+        dispatch(SetPicture, {date: props.date, picture})
+
+        // Pre-load image in the browser
+        new Image().src = picture.url
+      })
+      .catch(err => console.log('Fetch error: ', err)),
   date: props.date,
 })
 
-
-
-
-
-const hashchangeEffect = (props, dispatch) => {
-  const eventListener = event => {
-    const fallback = relativeDateString(new Date().toISOString().split('T')[0], -1)
-    const newDate = window.location.hash.substring(2) || fallback
-    const prevDate = event.oldURL.substring(0, event.oldURL.indexOf("#/")) || fallback
-    dispatch(props.action, newDate, prevDate)
-  }
-  addEventListener("hashchange", eventListener)
-  return () => removeEventListener("hashchange", eventListener)
-}
-
+// Listen to hash changes in the browser
 export const LocationChanged = props => ({
-  effect: hashchangeEffect,
+  effect: (props, dispatch) => {
+    const eventListener = event => {
+      const fallback = relativeDateString(new Date().toISOString().split('T')[0], -1)
+      const newDate = window.location.hash.substring(2) || fallback
+      const prevDate = event.oldURL.substring(0, event.oldURL.indexOf("#/")) || fallback
+      dispatch(props.action, newDate, prevDate)
+    }
+    addEventListener("hashchange", eventListener)
+    return () => removeEventListener("hashchange", eventListener)
+  },
   action: props.action
 })
-
-
 
 // Returns a YYYY-MM-DD date string, incremented or decremented
 // by a specified amount of days (direction).
